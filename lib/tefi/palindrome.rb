96fbe660acff44cd21403133cdabb9929ad3a1f2
tefi-anbessa <frankrobinson99@hotmail.com>
2020-09-15 12:26:27 +0700

    Initialise repository

---
+++ lib/tefi/palindrome.rb
@@ -0,0 +1,8 @@
+require "tefi/palindrome/version"
+
+module Tefi
+  module Palindrome
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/tefi/palindrome/version.rb
@@ -0,0 +1,5 @@
+module Tefi
+  module Palindrome
+    VERSION = "0.1.0"
+  end
+end
